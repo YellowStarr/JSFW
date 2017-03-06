68d75522a915791b31bacf5d423023cb1f3c7ace
AzureQiou <YellowStarr@users.noreply.github.com>
2017-03-06 11:08:31 +0800

    Add files via upload

---
+++ all_tests.py
@@ -0,0 +1,23 @@
+#codidng=utf-8
+import unittest
+import sys,datetime,time
+sys.path.append("testcase")
+from testcase import *
+import HTMLTestRunner
+
+import login_test
+
+caseNames=[
+    login_test.Login_Test
+]
+
+testunit=unittest.TestSuite()
+for i in range(0,len(caseNames)):
+    testunit.addTest(unittest.makeSuite(caseNames[i]))
+
+nowtime=time.strftime("%Y-%m-%M-%H_%M_%S",time.localtime(time.time()))
+filename='reporter\\'+nowtime+"reporter.html"
+fp=file(filename,'wb')
+runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title='testcase-logintest',description='desc')
+
+runner.run(testunit)
